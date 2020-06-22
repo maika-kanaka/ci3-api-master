e70cc24430b1a2d7fcf5b6011dc01abc82768b7c
Maika Kanaka <maika-kanaka@protonmail.com>
2020-06-22 16:43:41 +0700

    api for update user group

--- application/controllers/Api/Sys/Group.php
+++ application/controllers/Api/Sys/Group.php
@@ -15,19 +15,32 @@ class Group extends My_Controller
 
 	public function data()
 	{
-		$groups = $this->User_group_model
+		$query = $this->User_group_model
 			->table()
 			->order_by('group_name');
 
 		if (!empty($this->input->get('is_active'))) {
-			$groups->where('is_active', $this->input->get('is_active'));
+			$query->where('is_active', $this->input->get('is_active'));
 		}
 
-		$groups = $groups->get()->result();
+		if( !empty($this->input->get('group_id')) ){
+			$query->where('group_id', $this->input->get('group_id'));
+		}
 
+		$groups = $query->get()->result();
+
+		# ambil 
+		$my_page_access = null;
+        if (!empty($this->input->get('group_id'))) {
+			$my_page_access = $this->User_group_access_model
+									->table()
+									->where('group_id', $this->input->get('group_id'))
+									->get()
+									->result();
+        }
 
 		echo json_encode([
-			'data' => ['groups' => $groups],
+			'data' => ['groups' => $groups, 'my_page_access' => $my_page_access],
 			'errors' => null
 		]);
 		set_status_header(200);
@@ -38,13 +51,29 @@ class Group extends My_Controller
 		# param 
 		$raw_input = file_get_contents("php://input");
 		$input = json_decode($raw_input);
+		$page = $input->page;
 
-		$not_duplicat = true;
-		$query = $this->User_group_model->find(['group_name' => trim($input->group_name)]);
-		if(!empty($query)){
-			$not_duplicat = false;	
+		if($page == 'add')
+		{
+			$not_duplicat = true;
+			$query = $this->User_group_model->find(['group_name' => trim($input->group_name)]);
+			if(!empty($query)){
+				$not_duplicat = false;	
+			}
 		}
-        
+		else if($page == 'edit')
+		{
+			$not_duplicat = true;
+
+			$data_before = $this->User_group_model->find(['group_id' => trim($input->group_id)]);
+			if( trim(strtolower($data_before->group_name)) != strtolower(trim($input->group_name)) )
+			{
+				$query = $this->User_group_model->find(['group_name' => trim($input->group_name)]);
+				if(!empty($query)){
+					$not_duplicat = false;	
+				}
+			}
+		}
 
 		echo json_encode([
 			'ok' => $not_duplicat
@@ -81,6 +110,53 @@ class Group extends My_Controller
 		set_status_header(200);
 	}
 
+	public function update()
+	{
+		// param 
+		$raw_input = file_get_contents('php://input');
+		$objInput = json_decode($raw_input);
+
+		// data sebelumnya 
+		if( empty($objInput->group_id) ){
+			echo json_encode([
+				'errors' => ['GROUP ID IS INVALID']
+			]);
+			set_status_header(403);
+			exit;
+		}
+		$data_before = $this->User_group_model->find(['group_id' => $objInput->group_id]);
+
+		// tangkap input 
+		$input = $this->_get_input('edit');
+
+		// valid nama double
+		if( trim(strtolower($data_before->group_name)) != trim(strtolower($input['header']['group_name'])) )
+		{
+			$duplicat = $this->User_group_model->find(['group_name' => $input['header']['group_name']]);
+			if (!empty($duplicat)) {
+				echo json_encode([
+					'errors' => ['Nama kelompok <b> '. $input['header']['group_name'] . ' </b> sudah ada.']
+				]);
+				set_status_header(403);
+				exit;
+			}
+		}
+
+		// proses simpan
+		$this->db->trans_start();
+			$this->User_group_model->update(['group_id' => $objInput->group_id], $input['header']);
+			$this->db->delete($this->User_group_access_model->table, ['group_id' => $objInput->group_id]);
+			$this->User_group_access_model->insert($input['menus']);
+		$this->db->trans_complete();
+
+		// alihkan 
+		echo json_encode([
+			'data' => null,
+			'errors' => null
+		]);
+		set_status_header(200);
+	}
+
 	private function _get_input($evt = 'add')
 	{
 		# param
@@ -91,6 +167,8 @@ class Group extends My_Controller
 		# get header
 		if ($evt == 'add') {
 			$insert['group_id'] = $this->User_group_model->primaryKeyInc();
+		}else{
+			$insert['group_id'] = $input->group_id;
 		}
 		$insert['group_name'] = trim($input->group_name);
 
